b71d9756d5e0cd0b3dbde8ab60afe12e1dc12651
shankar Ghimire <ssghimire@gmail.com>
2021-11-15 13:49:09 -0500

    first commit

---
+++ src/main/java/com/springbootdatabase/SpringbootdatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.springbootdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdatabaseApplication.class, args);
+		
+	}
+
+}
